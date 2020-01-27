2537e1f945f2f72173e455b136a05ce20c18bf30
github username <github email address>
2020-01-26 21:10:22 -0500

    next commit

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-      <p>Welcome to React Vending Machine! Deposit some money and help yourself to a snack or two!</p>
+      <p>Welcome to the React Vending Machine! Deposit some money and help yourself to a snack or two!</p>
     </div>
   );
 }
